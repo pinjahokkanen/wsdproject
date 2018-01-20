7a49662544a6cdc52cc245dba533cb5675830f58
Pinja Hokkanen <pinja.hokkanen@aalto.fi>
2018-01-20 22:49:53 +0200

    Created the Profile model which extends the user model, and made some settings to make the signup form work

--- src/webapp/models.py
+++ src/webapp/models.py
@@ -1,18 +1,23 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+
+    def __unicode__(self):
+        return str(self.user.username) #How viewed in django admin, same as __str__ in python2
 
 @receiver(post_save, sender=User)
-def update_user_profile(sender, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
 # Setting the name and description of the game. All the names are unique, and primary key (game id, referenced pk) is created automatically by django.
--- src/webapp/views.py
+++ src/webapp/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-
 from webapp.templates.forms import SignUpForm
 
 
