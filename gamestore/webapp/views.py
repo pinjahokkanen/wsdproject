8c96104101aea68857f285964fb413b9f1cca0a7
Pinja Hokkanen <pinja.hokkanen@aalto.fi>
2018-01-20 15:44:52 +0200

    Implemented first index view and template

--- gamestore/webapp/views.py
+++ gamestore/webapp/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
+def index(request):
+    return render(request, "webapp/index.html", {})
 # Create your views here.
