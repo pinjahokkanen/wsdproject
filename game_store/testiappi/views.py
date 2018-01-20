c633fadc0ff0e51e71b6b82558e24212b5460fca
Pinja Hokkanen <pinja.hokkanen@aalto.fi>
2018-01-20 13:09:30 +0200

    Fresh start

--- game_store/testiappi/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
-
-def about(request):
-    return render(request, "testiappi/home.html", {})
