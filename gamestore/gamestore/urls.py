42730532bd2ca483df81c509b617443af987dd2e
Leevi Lappi <leevi.lappi@aalto.fi>
2018-01-20 13:40:27 +0200

    Heroku template installed

--- gamestore/gamestore/urls.py
+++ gamestore/gamestore/urls.py
@@ -1,4 +1,4 @@
-"""game_store URL Configuration
+"""gamestore URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+
 from django.contrib import admin
 from django.urls import path
-from testiappi import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^about/$', views.about, name='about'),
 ]
--- gamestore/gamestore/wsgi.py
+++ gamestore/gamestore/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for game_store project.
+WSGI config for gamestore project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "game_store.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamestore.settings")
 
 application = get_wsgi_application()
