eaf39e0967a7b0927400a340cb5d942af31d7942
Pinja Hokkanen <pinja.hokkanen@aalto.fi>
2018-01-20 21:29:38 +0200

    Created a Game model, and a rough User model to be referenced to

--- gamestore/webapp/models.py
+++ gamestore/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Setting the name and description of the game. All the names are unique, and primary key (game id, referenced pk) is created automatically by django.
+# States - don't know what this does? Was in the project plan.
+# Added a developer field - foreign key reference to one User (will need to create the user model) that also needs to be a developer. When the user (developer) is deleted, this field is set to NULL (instead of deleting the game). Can also be an admin/platform owner this point, and game would be free?
+# Highcores defined as a ManyToMany - relationship (a game has several highscores from different players (always player's highest score)). No need for a seperate model, since highscores referenced with players_highscores from the User model.
+class Game(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    states = models.IntegerField() #WHAT IS THIS?!?!?!?!
+    url = models.URLField()
+    price = models.DecimalField(max_digits=10, decimal_places=2) #Changed the float to decimal due to rounding issues
+    developer = models.ForeignKey('User', related_name='developed_games', null=True, on_delete=models.SET_NULL)
+    highscores = models.ManyToManyField('User', related_name='highscores', related_query_name='scores')
+
+class User(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    #had to do something in order to make game - model work
