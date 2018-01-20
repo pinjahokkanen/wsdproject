eec4f6eaaf145f8eb60c50ddb91b761583224a99
Pinja Hokkanen <pinja.hokkanen@aalto.fi>
2018-01-20 16:22:21 +0200

    One more time

--- gamestore/webapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
-
-def index(request):
-    return render(request, "webapp/index.html", {})
-# Create your views here.
