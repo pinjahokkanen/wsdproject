a84ce3a968f8315e8b2dc2b57bfb22c78e5d5f53
Pinja Hokkanen <pinja.hokkanen@aalto.fi>
2018-02-16 23:41:02 +0200

    Renamed src => gamestore and moved procfile, requirements.txt and runtime

--- gamestore/gamestore/settings.py
+++ gamestore/gamestore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hxmxwy11a$=5oi!w5d901)3e%=+ej27#-ary_8$bw_8=*ld6ed'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = []
 
 
 # Application definition
